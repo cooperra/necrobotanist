6151be28b06db41d9b43bf9f41ed4b62953f35dd
Robbie Cooper <cooperra@users.noreply.github.com>
2022-01-23 03:53:40 -0500

    Fix false-firing of falloff signal

--- Entities/Necrobotanist/Necrobotanist.gd
+++ Entities/Necrobotanist/Necrobotanist.gd
@@ -53,7 +53,7 @@ func _ready():
 func _physics_process(delta):
 	handle_horiz_input(delta)
 	handle_jump_state(delta)
-	velocity = move_and_slide(velocity, up_direction)
+	velocity = move_and_slide_with_snap(velocity, up_direction.rotated(PI), up_direction)
 	handle_collisions()
 
 
